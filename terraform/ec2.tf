56afdee7d45f4a6aa5b5947ecd3c3522c992637e
bmukum <bmukum99@gmail.com>
2023-04-03 18:05:07 -0400

    ami

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test_instance" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-00c39f71452c08778"
   instance_type = "t2.nano"
   tags = {
     Name = "test_instance"
