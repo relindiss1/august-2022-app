7588f32951aa4a43273877056087da56027d0642
bmukum <bmukum99@gmail.com>
2023-04-05 20:07:04 -0400

    format

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "docker" {
-  ami  = "ami-06e46074ae430fba6"
-  count = 2
+  ami           = "ami-06e46074ae430fba6"
+  count         = 2
   instance_type = "t2.micro"
-  key_name = "docker-demo"
+  key_name      = "docker-demo"
 
   tags = {
     Name = "docker-${count.index}"
